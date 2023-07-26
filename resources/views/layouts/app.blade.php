aec651f7cf29193e42d6a472a2fe3650961a93fc
adisonben <adison.ben.2543@gmail.com>
2023-07-26 17:14:22 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,63 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/css/app.css' , 'resources/js/app.js'])
+
+    <style>
+        :root{
+    --bs-dark: rgb(242, 255, 255);
+  }
+  
+  .theme-container {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.5s;
+  }
+  
+  .theme-container:hover {
+    opacity: 0.8;
+  }
+  
+  @keyframes change {
+    0% {
+      transform: scale(1);
+    }
+  
+    100% {
+      transform: scale(1.4);
+    }
+  }
+  
+  .change {
+    animation-name: change;
+    animation-duration: 1s;
+    animation-direction: alternate;
+  }
+  .bi-moon-stars{
+    color: white;
+  }
+  .nav-light{
+    background-color: white;
+    color: black;
+  }
+
+  .nav-dark{
+    background-color: black;
+    color: white;
+  }
+  .nav-dark .navbarMenu{
+    color: white;
+  }
+  .nav-dark .navbarMenu:hover{
+    color: pink;
+  }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -120,4 +177,61 @@
         </h5>
     </div>
 </body>
+
+<script>
+    document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+const sun = "bi-sun";
+const moon = "bi-moon-stars"
+
+var theme = "light";
+const root = document.querySelector(":root");
+const container = document.getElementsByClassName("theme-container")[0];
+const themeIcon = document.getElementById("theme-icon");
+const navb = document.getElementById("navb");
+container.addEventListener("click", setTheme);
+
+function setTheme() {
+    switch (theme) {
+        case "dark":
+            setLight();
+            theme = "light";
+            break;
+        case "light":
+            setDark();
+            theme = "dark";
+            break;
+    }
+}
+
+function setLight() {
+    root.style.setProperty(
+        "--bs-dark",
+        "rgb(242, 255, 255)"
+    );
+    container.classList.remove("shadow-dark");
+    themeIcon.classList.remove(moon);
+    navb.classList.remove("nav-dark");
+    setTimeout(() => {
+        container.classList.add("shadow-light");
+        themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    navb.classList.add("nav-light");
+    themeIcon.classList.add(sun);
+}
+
+function setDark() {
+    root.style.setProperty("--bs-dark", "#212529");
+    container.classList.remove("shadow-light");
+    themeIcon.classList.remove(sun);
+    navb.classList.remove("nav-light");
+    setTimeout(() => {
+        container.classList.add("shadow-dark");
+        themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    navb.classList.add("nav-dark");
+    themeIcon.classList.add(moon);
+}
+</script>
 </html>
