27184e83aeaac2e0c69ad8aa9c84bfc42f768a4c
adisonben <adison.ben.2543@gmail.com>
2023-08-11 14:25:43 +0700

    fx

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('dist/img/logoiddrives.png') }}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
