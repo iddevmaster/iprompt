e40edad74900125aa5abde5099c6921292388710
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-09-22 14:44:50 +0700

    fx

--- app/Http/Controllers/TablesController.php
+++ app/Http/Controllers/TablesController.php
@@ -211,7 +211,7 @@ class TablesController extends Controller
 
         foreach($gendoc as $doc) {
             if ($doc->dpm === '-'){
-                dd(is_array(json_decode($doc->submit_by)));
+                dd(is_array(json_decode("1")));
                 $doc->dpm = (department::find((User::find($doc->submit_by))->dpm))->prefix;
                 $doc->save();
             }
