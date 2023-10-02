4d7abe4434671e2d38ee7ff3a704551c91314888
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-10-02 13:36:07 +0700

    fx

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -201,7 +201,7 @@ class HomeController extends Controller
 
             return response()->json(['data' => $data, 'user' => $user]);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'data is not save']);
+            return response()->json(['error' => 'data is not save']);
         }
     }
 
