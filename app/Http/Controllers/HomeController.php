3f3e8faeda927e8e5f42782ba4b1bb76c8b17fe9
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-10-02 13:33:19 +0700

    fx

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -197,7 +197,7 @@ class HomeController extends Controller
             $user->syncRoles([$data['role']]);
             $user->role = $data['role'];
 
-            // $user->save();
+            $user->save();
 
             return response()->json(['data' => $data, 'user' => $user]);
         } catch (\Exception $e) {
