48f76c0257232dc8b034277909e8168db847a4d1
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-09 09:11:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
