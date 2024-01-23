08cbcf02ea3a0f6d18689f7ee296637883f7401d
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-23 16:48:08 +0700

    Create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!(User::find('email', 'admin'))) {
+            $user = User::create([
+                'name' => 'admin',
+                'email' => 'admin',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'agency' => '0',
+                'branch' => '0',
+                'phone' => 'xxxxxxxx',
+                'dpm' => '0',
+            ]);
+
+            $user->assignRole('admin');
+        } else {
+            $user = User::find('email', 'admin');
+            $user->assignRole('admin');
+        }
+    }
+}
