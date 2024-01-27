0e4fa15753c38a574f19e387ddf05f319351fa01
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-27 16:04:46 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,37 +10,35 @@
             return $contract->alert == 1;
         }));
     @endphp
-    @if ($total)
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">การแจ้งเตือน</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>คุณมีสัญญา <span class="badge text-bg-danger">{{ $total }}</span> ฉบับที่กำลังจะหมดอายุ</p>
-                        @foreach ($contracts->filter(function ($contract) {
-                            return $contract->alert == 1;
-                        }) as $index => $contract)
-                            @php
-                                $dateArray = explode(' - ', $contract->time_range);
-                                $endDate = Carbon\Carbon::createFromFormat('d/m/Y', $dateArray[1]);
-                                $currentDate = Carbon\Carbon::now();
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">การแจ้งเตือน</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>คุณมีสัญญา <span class="badge text-bg-danger">{{ $total }}</span> ฉบับที่กำลังจะหมดอายุ</p>
+                    @foreach ($contracts->filter(function ($contract) {
+                        return $contract->alert == 1;
+                    }) as $index => $contract)
+                        @php
+                            $dateArray = explode(' - ', $contract->time_range);
+                            $endDate = Carbon\Carbon::createFromFormat('d/m/Y', $dateArray[1]);
+                            $currentDate = Carbon\Carbon::now();
 
-                                $daysDifference = $currentDate->diffInDays($endDate);
-                            @endphp
-                            <div class="d-flex justify-content-between alert alert-warning" role="alert">
-                                <p class="mb-0 text-wrap text-break border-end px-2 flex-fill border-black">{{ $contract->book_num }} :: {{ $contract->title }}</p>
-                                <p class="mb-0 text-nowrap align-self-center px-2"><i class="bi bi-hourglass-split" style="font-size: 20px"></i> {{ $daysDifference }} วัน</p>
-                            </div>
-                        @endforeach
-                    </div>
+                            $daysDifference = $currentDate->diffInDays($endDate);
+                        @endphp
+                        <div class="d-flex justify-content-between alert alert-warning" role="alert">
+                            <p class="mb-0 text-wrap text-break border-end px-2 flex-fill border-black">{{ $contract->book_num }} :: {{ $contract->title }}</p>
+                            <p class="mb-0 text-nowrap align-self-center px-2"><i class="bi bi-hourglass-split" style="font-size: 20px"></i> {{ $daysDifference }} วัน</p>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
-    @endif
+    </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mb-5 d-flex justify-content-center">
