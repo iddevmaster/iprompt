3e5d3b500bf65db52ec9613d757e4e27fb84311d
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-06-13 14:40:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,10 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-// Route::get('/phpinfo', function () {
-//     phpinfo();
-// });
-
 Auth::routes();
-
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/alluser', [App\Http\Controllers\HomeController::class, 'alluser'])->name('alluser');
 Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
